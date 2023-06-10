65ada50a64f70978e5a0caa1ab8b5dd4200ac791
MiriamLaw <129810725+MiriamLaw@users.noreply.github.com>
2023-06-10 08:43:38 -0700

    Version Check

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,10 +1,37 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Random random = new Random();
+		int randomNumber = random.nextInt(100) + 1;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int guesses = 1; guesses <= 5; guesses++ ) {
+		
+		System.out.println("Pick a number between 1 and 100");
+		
+		
+		
+		System.out.println("Your guess is not between 1 and 100, please try again");
+		
+		System.out.println("Please pick a higher number");
+		
+		System.out.println("Please pick a lower number");
+		
+		System.out.println("You win!");
+		
+		System.out.println("You lose. The number to guess was: " + randomNumber);
 
 	}
+		scanner.close();
+	
+		
 
+}
 }
